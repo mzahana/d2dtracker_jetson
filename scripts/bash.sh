e7974cff527fe699349f07cecc47a6f4d67238e0
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-09-01 14:34:05 +0300

    Create convenience aliases in the bash.sh script

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -15,3 +15,28 @@ export RUN_KF=False
 export RUN_TRAJ_PRED=False
 export RUN_YOLO=False
 export RUN_YOLO_POSE=False
+
+
+# The following code creates convenience aliases to be used inside the d2dtracker container
+bashrc_file="$HOME/.bashrc"
+line_to_check="alias viz='rviz2 -d /home/d2d/shared_volume/ros2_ws/src/open_vins/ov_msckf/launch/display_ros2.rviz'"
+
+if ! grep -qF "$line_to_check" "$bashrc_file"; then
+    echo "$line_to_check" >> "$bashrc_file"
+fi
+
+bashrc_file="$HOME/.bashrc"
+line_to_check="alias rs='ros2 launch realsense2_camera rs.launch.py'"
+
+if ! grep -qF "$line_to_check" "$bashrc_file"; then
+    echo "$line_to_check" >> "$bashrc_file"
+fi
+
+bashrc_file="$HOME/.bashrc"
+line_to_check="alias viz='rviz2 -d /home/d2d/shared_volume/ros2_ws/src/open_vins/ov_msckf/launch/display_ros2.rviz'"
+
+if ! grep -qF "$line_to_check" "$bashrc_file"; then
+    echo "$line_to_check" >> "$bashrc_file"
+fi
+
+source $HOME/.bashrc
